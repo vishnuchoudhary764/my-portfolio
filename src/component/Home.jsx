0e66fd17fadc1213f9e132b13c778a285b2c8906
vishnuchoudhary764 <vasanaram162@gmail.com>
2025-09-30 20:18:24 +0530

    eresume

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -8,10 +8,17 @@ const Home = () => {
                 <h2 className='typewritter'>Frontend Developer</h2>
                 <p className='passion'>I'm a frontend developer with strong foundation in HTML, CSS, JavaScript, and modern frameworks like React.js. Skilled in building responsive, user-friendly web interfaces with a focus on performance and accessibility.Eager to contribute to dynamic teams, continuously learn new technologies.Let's start scrolling and learn more about me.</p>
                 <div className="btns">
-                    <button className='rbtn'>Resume</button>
+                  
+                    <a
+                        href="/my_resume.pdf"
+                        download="Vishnu_Choudhary.pdf"
+                        className="bg-blue-600 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-700 transition"
+                    >
+                         <button className='rbtn'>Resume</button>
+                    </a>
                     <button className='hbtn'>Hire Me</button>
                 </div>
-               
+
             </div>
         </div>
     )
